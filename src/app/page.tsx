56295c08baf579046a646cc15e7d3fb53459abe6
Jonathan Li <87139436+JonathanLi-git@users.noreply.github.com>
2023-11-02 20:33:17 -0400

    3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,12 @@ import { useRouter } from "next/navigation";
 import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
 
-
 export default function Home({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   return (
-    <div>adsf</div>
+    <div className='mt-36 ml-36 w-36 bg-purple-600 transition duration-150 ease-out hover:scale-105'>
+      <button className="text-white" onClick={() => router.push('/login')}>Click here to create account and login</button>
+    </div>
   );
 }
