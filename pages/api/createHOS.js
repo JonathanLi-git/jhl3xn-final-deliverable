61f2543f9674ecb6332d75c5d8877043ab2bb4b6
Jonathan Li <87139436+JonathanLi-git@users.noreply.github.com>
2023-12-03 15:31:49 -0500

    Update createHOS.js

--- pages/api/createHOS.js
+++ pages/api/createHOS.js
@@ -7,6 +7,7 @@ import { query } from "./queries";
 export default async function handler(req, res) {
   if (req.method == "POST") {
     const params = req.body.params
+    
     const queryString = "CALL createHOS(?, ?);";
     const result = await query({ query: queryString, values: params });
     res.status(200).json({ data: result });
